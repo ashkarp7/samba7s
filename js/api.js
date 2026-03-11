7834ef96a277ac888ece47d2e1a240449332004d
MOHAMMED ASHKAR P <mohammedashkar26@gmail.com>
2026-03-11 10:30:14 +0530

    fix: revert api url to production

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "https://samba7s.onrender.com";
 
 // Fires instantly on page load to wake Render out of cold start
 // before any real data request is made.
